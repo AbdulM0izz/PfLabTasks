95d1e146b477bf8b9a047790d915281b9c9f274d
Abdul Moiz <abdulmoiz95972@gmail.com>
2024-11-19 13:56:49 +0500

    Done

--- Weekly challenge 2/invertedpyramud4.cpp
+++ Weekly challenge 2/invertedpyramud4.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int n=5;
-    for (int i= 0; i < n ; i++){
-        //spaces
-        for (int j = i ; j <= i ; j ++){
-            if(i != 0){
-                cout << " ";
-            }
+    int n= 9;
+   for (int i = 0 ; i < n ; i++){
+    //spaces
+    for (int j = 0; j < i; j++){
+        if(i != 0){
+            cout << " ";
         }
-        //stars
-        for(int j = n ; j > 0 ; j--){
-            cout << "* ";
-        }
-        cout << endl;
+    }
+    //star
+    for(int j = 1; j <= n - i; j++){
+        cout << "* ";
     }
 
-
-
+    cout << endl;
+   }
+     
     return 0;
 }
